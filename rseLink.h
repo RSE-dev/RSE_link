d9e306726830663c53ea0ddd2ed337ddb1659092
RSE-dev <145577539+RSE-dev@users.noreply.github.com>
2025-06-03 00:15:44 +0300

    Update rseLink.h

--- rseLink.h
+++ rseLink.h
@@ -22,7 +22,8 @@ typedef enum {
 
 typedef enum {
     PC_HOST_ID = 0,
-    RSE_LEX_MASTERCOM_ID = 1
+    RSE_LEX_MASTERCOM_CH1_ID = 1,
+    RSE_LEX_MASTERCOM_CH2_ID = 2
 } RSE_SYSTEM_ID;
 
 /* ComponentID ranges: */
